00447e5cc8cd2621fd4b97bd4fd49fb736041667
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-02-26 12:38:26 -0600

    task 1

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 
-export class App extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <Link>Go Home</Link>
+          <Link>View Films</Link>
+          <Link>View People</Link>
+          <Switch>
+            <Route />
+            <Route />
+          </Switch>
+        </Fragment>
+      </Router>
+    );
+  }
 }
 
-export default App
+export default App;
